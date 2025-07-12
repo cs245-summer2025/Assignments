a618b28cc124015694eaebedc251f4b7a80467f7
aprakash3 <135915407+aprakash3@users.noreply.github.com>
2025-07-12 11:18:59 -0700

    Add files via upload

---
+++ Hashing/src/map/MyMap.java
@@ -0,0 +1,32 @@
+package map;
+
+public class MyMap<K, V> {
+    // Define any attributes here
+
+    public MyMap(){
+        // Fill in constructor
+    }
+
+    public void put(K key, V value){
+        // TODO
+    }
+
+    public boolean contains(K key){
+        // TODO
+        return false;
+    }
+
+    public V remove(K key){
+        // TODO
+        return null;
+    }
+
+    public V get(K key){
+        // TODO
+        return null;
+    }
+
+    public void resize(){
+        // TODO
+    }
+}
